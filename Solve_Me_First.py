04c7a00fcfa431eb6f6523163a4adab959a5767d
Sudhesh Solomon <sudhesh.solomon1992@gmail.com>
2020-02-17 11:07:53 +0530

    Create Solve_Me_First.py

---
+++ Solve_Me_First.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+def main():
+    first_number = int(input())
+    second_number = int(input())
+    solveMeFirst(first_number, second_number)
+
+def solveMeFirst(first_number, second_number):
+    print (first_number + second_number)
+
+if __name__ == "__main__":
+    main()
+
